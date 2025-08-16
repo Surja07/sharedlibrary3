7adc95b1d877c5bf1906fad6841ff929bc67897e
Surja07 <153250264+Surja07@users.noreply.github.com>
2025-08-16 15:14:50 +0530

    Update cicdds.groovy

--- vars/cicdds.groovy
+++ vars/cicdds.groovy
@@ -11,4 +11,14 @@ def deployment(jobname,ip)
   sh "scp /var/lib/jenkins/workspace/${jobname}/HelloWorld.java ubuntu@${ip}:/home/ubuntu/demo4"
 }
 
+def test(jobname)
+{  
+  sh """
+    javac /var/lib/jenkins/workspace/${jobname}/HelloWorld.java
+    java -cp /var/lib/jenkins/workspace/${jobname} HelloWorld > /var/lib/jenkins/workspace/${jobname}/test_output.txt
+"""
+}
+
+
+
   
